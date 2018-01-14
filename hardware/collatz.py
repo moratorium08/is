ffc5782c3254ab64f89ebe7bd6831ebfa4f21497
moratorium08 <moratorium08@gmail.com>
2018-01-14 12:00:40 +0900

    Use Trace Cache

--- hardware/collatz.py
+++ hardware/collatz.py
@@ -15,13 +15,28 @@ name2 = 0
 name3 = 0
 name4 = 0
 
+clock = 0
+
+CACHE_SIZE = 1024
+TRACE_CHACHE_ON = True
+
+# 途中通った時にどっから始めた回か？
+trace = [0 for i in range(CACHE_SIZE)]
+# 途中通った時にそれまでの最大値
+tracemx = [0 for i in range(CACHE_SIZE)]
+# 最初からのときは最後まで言った時の最大値
+memmx = [0 for i in range(CACHE_SIZE)]
+memlen = [0 for i in range(CACHE_SIZE)]
+
 for i in range(1, 1024, 2):
-    # print("searching...", i)
+#for i in range(1023, 2, -2):
+    print("searching...", i)
     data = i
     cnt = 0
     mx = data
     while data != 1:
-        # print(data)
+        clock += 1
+        #print(data)
         m = data % 4
         if data == 2:
             cnt += 1
@@ -41,6 +56,36 @@ for i in range(1, 1024, 2):
             mx = max(3 * data + 1, mx)
             data = (3 * data + 1) // 2
             cnt += 2 # ここもう少し勧められるが...
+
+        if data < CACHE_SIZE:
+            if memmx[data] != 0:
+                if trace[data] == 0:
+                    mx = max(mx, memmx[data])
+                    cnt += memlen[data]
+                    data = 1
+                else:
+                    # Traceしてdataまでたどり着いたときの最大値
+                    # より、最後まで言った時(trace[data])
+                    # の最大値の方がでかかったとき
+                    if memmx[data] < memmx[trace[data]]:
+                        print('hit')
+                        mx = max(mx, memmx[trace[data]])
+                        cnt += (memlen[trace[data]] - memlen[data])
+                        data = 1
+                    elif mx >= memmx[data]:
+                        print('hit')
+                        data = 1
+                        cnt += (memlen[trace[data]] - memlen[data])
+                    else:
+                        print('諦めた')
+            elif TRACE_CHACHE_ON:
+                memmx[data] = mx
+                memlen[data] = cnt
+                trace[data] = i
+
+    memmx[i] = mx
+    memlen[i] = cnt
+    trace[i] = 0
     if mx1 == mx:
         if len1 < cnt:
             len1 = cnt
@@ -93,7 +138,16 @@ for i in range(1, 1024, 2):
         len4 = cnt
         name4 = i
 
+'''
+41524 131 639
+118096 62 1023
+190996 178 871
+250504 173 937
+'''
 print(name1, name2, name3, name4)
+print(len1, len2, len3, len4)
+print(mx1, mx2, mx3, mx4)
+print('clock', clock)
 
 
 
